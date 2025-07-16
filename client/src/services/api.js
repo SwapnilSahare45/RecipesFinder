9b311a64a927f03282d16a0b14902571d433db75
Swapnil Sahare <swapnilsahare45@gmail.com>
2025-07-16 17:36:05 +0530

    package fix

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
 
 const api = axios.create({
-    baseURL: "http://localhost:4500/api",
+    baseURL: process.env.API,
 });
 
 export default api;
