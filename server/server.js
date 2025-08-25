a4d957ab8f072fa67e6dcf9a9935d37b23c34e34
Swapnil Sahare <swapnilsahare45@gmail.com>
2025-08-25 15:31:50 +0530

    add env client url

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ dbConnect();
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.CLIENT_URL,
     credentials: true,
 }));
 app.use(express.json());
